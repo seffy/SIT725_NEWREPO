1eefc67b3eb4cdd7563e35cb994ac46b20842dd0
Joseph Jhon Sabana <seffy.sabana@gmail.com>
2025-01-06 00:27:57 +1100

    Week 7

---
+++ Week7/public/js/script.js
@@ -0,0 +1,44 @@
+const socket = io('http://localhost:8080');
+const messagesDiv = document.getElementById('messages');
+const messageInput = document.getElementById('messageInput');
+
+// Real-time updates
+socket.on('message-added', (message) => {
+  renderMessage(message); // Prepend the new message
+});
+
+socket.on('messages-updated', (sortedMessages) => {
+  messagesDiv.innerHTML = ''; // Clear the current list
+
+  sortedMessages.forEach(renderMessage); // Render each message in the correct order
+});
+
+socket.on('message-deleted', () => {
+  fetchMessages(); // Refresh all messages
+});
+
+
+// Listen for random number updates
+socket.on('number', (msg) => {
+  console.log('Random number: ' + msg); // Log the random number to the console
+});
+
+
+
+
+
+$(document).ready(function(){
+  $('.modal').modal();
+
+    // Close modal on submit button click
+    $('#submit-button').click(function() {
+      $('#modal1').modal('close'); // Replace 'modal-id' with the ID of your modal
+    });
+
+  // Close modal on submit button click
+  $('#modal-close').click(function() {
+    $('#modal1').modal('close'); // Replace 'modal-id' with the ID of your modal
+  });
+
+
+});
